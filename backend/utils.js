2b94521bb258fc1a7fea95e30176f85d0ac0d225
prasanv <prasan.venkatb@gmail.com>
2022-08-28 22:15:01 -0700

    updated utils

--- backend/utils.js
+++ backend/utils.js
@@ -1,9 +1,12 @@
-const dotenv = require("dotenv").config({ path: "../backend/.env" });
+require("dotenv").config({ path: "../backend/.env" });
 
-if (dotenv.error) throw dotenv.error;
+// Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+// The process object is an instance of Node.js EventEmitter.
+// Node.js `process.env` property returns an object containing the user environment
+// console.log(process.env);
 
-const DB_URI = dotenv?.parsed.DB_URI;
-const PORT = dotenv?.parsed.PORT;
+const DB_URI = process.env?.DB_URI;
+const PORT = process.env?.PORT;
 
 module.exports = {
   DB_URI,
