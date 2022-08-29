352dd13a99af079b483e0490e93d0d5e160222ca
prasanv <prasan.venkatb@gmail.com>
2022-08-28 23:55:00 -0700

    minor changes to userRouter

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -4,6 +4,11 @@ const User = require("../models/user.model");
 // A router object is an isolated instance of middleware and routes. You can think of it as a “mini-application,” capable only of performing middleware and routing functions. Every Express application has a built-in app router.
 // A router behaves like middleware itself, so you can use it as an argument to app.use() or as the argument to another router’s use() method.
 
+// Model Methods Example
+// For the full list of Model methods and options check Mongoose documentation
+// User.find({username : "Prasan"}).then(res => console.log(res));
+
+// GET ALL USERS
 router.get("/", (req, res) => {  
   User.find()
     .then((data) => res.json(data))
@@ -11,9 +16,7 @@ router.get("/", (req, res) => {
 });
 
 
-// For the full list of Model methods and options check Mongoose documentation
-// User.find({username : "Prasan"}).then(res => console.log(res));
-
+// ADD NEW USER
 router.post("/add", (req, res) => {
   const newUser = req.body;
   const user = new User(newUser);
